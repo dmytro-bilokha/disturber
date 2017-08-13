6c8666569bee08ddffe476724e5741359cb1e1b9
Dmytro Bilokha <dmytro@posteo.net>
2017-08-13 11:41:09 +0300

    Added ListView with mock data and echo

--- disturber-jar/src/main/java/com/dmytrobilokha/disturber/controller/MainLayoutController.java
+++ disturber-jar/src/main/java/com/dmytrobilokha/disturber/controller/MainLayoutController.java
@@ -1,5 +1,10 @@
 package com.dmytrobilokha.disturber.controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,4 +18,24 @@ public class MainLayoutController {
 
     private static final Logger LOG = LoggerFactory.getLogger(MainLayoutController.class);
 
+    private ObservableList<String> messageList = FXCollections.observableArrayList("Test1", "Test2", "Test3");
+
+    @FXML
+    private ListView<String> messageListView;
+    @FXML
+    private TextArea messageTyped;
+
+    public MainLayoutController() {
+    }
+
+    @FXML
+    public void initialize() {
+        messageListView.setItems(messageList);
+        messageListView.refresh();
+    }
+
+    public void sendButtonHandler() {
+        messageList.add(messageTyped.getText());
+        messageTyped.clear();
+    }
 }
