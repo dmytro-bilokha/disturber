f59453052e79f79de6c551a0807f77f5f79852d0
Dmytro Bilokha <dmytro@posteo.net>
2017-09-13 19:49:49 +0300

    Added tests

--- disturber-jar/src/test/java/com/dmytrobilokha/disturber/appeventbus/AppEventBusTest.java
+++ disturber-jar/src/test/java/com/dmytrobilokha/disturber/appeventbus/AppEventBusTest.java
@@ -4,13 +4,17 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
+import static org.junit.Assert.assertEquals;
+
 public class AppEventBusTest {
 
     private AppEventBus eventBus;
+    private long eventCounter;
 
     @Before
     public void initBus() {
         eventBus = new AppEventBus();
+        eventCounter = 0;
     }
 
     @Test
@@ -35,8 +39,6 @@ public class AppEventBusTest {
         Mockito.verify(mockListener, Mockito.times(0)).onAppEvent(Mockito.anyObject());
     }
 
-    //TODO: implement test to check reference is weak
-
     @Test
     public void testClassifiesEvents() {
         AppEventListener<String, Void> classifiedMockListener = Mockito.mock(AppEventListener.class);
@@ -56,4 +58,38 @@ public class AppEventBusTest {
                 .onAppEvent(AppEvent.withClassifier(AppEventType.MATRIX_LOGGEDIN, "MOCK_LOGIN42"));
     }
 
+    @Test
+    public void testIgnoresRepeatedSubscription() {
+        AppEventListener<String, Void> mockListener = Mockito.mock(AppEventListener.class);
+        eventBus.subscribe(mockListener, AppEventType.MATRIX_LOGGEDIN);
+        eventBus.subscribe(mockListener, AppEventType.MATRIX_LOGGEDIN);
+        Mockito.verify(mockListener, Mockito.times(0)).onAppEvent(Mockito.anyObject());
+        eventBus.fire(AppEvent.withClassifier(AppEventType.MATRIX_LOGGEDIN, "MOCK_LOGIN2"));
+        Mockito.verify(mockListener, Mockito.times(1)).onAppEvent(Mockito.anyObject());
+    }
+
+    @Test
+    public void testWeakReferenceIsWeak() {
+        assertEquals(0L, eventCounter);
+        subscribeAndCheckEventGot();
+        System.gc(); //Trying to force JVM to collect unreferenced listener. Without GC executed the test will fail.
+        eventBus.fire(AppEvent.withClassifier(AppEventType.MATRIX_LOGGEDIN, "MOCK_LOGIN2"));
+        assertEquals(1L, eventCounter);
+    }
+
+    void subscribeAndCheckEventGot() {
+        AppEventListener countingListener = createListener();
+        eventBus.subscribe(countingListener, AppEventType.MATRIX_LOGGEDIN);
+        eventBus.fire(AppEvent.withClassifier(AppEventType.MATRIX_LOGGEDIN, "MOCK_LOGIN"));
+        assertEquals(1L, eventCounter);
+    }
+
+    private AppEventListener createListener() {
+        return new AppEventListener() {
+            @Override
+            public void onAppEvent(AppEvent appEvent) {
+                eventCounter++;
+            }
+        };
+    }
 }
