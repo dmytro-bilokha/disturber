73c5846895aa7d7959bc7a55ae53e53a19d361c6
Dmytro Bilokha <dmytro@posteo.net>
2018-08-18 20:09:02 +0300

    Add new properties to ignore due to matrix.org protocol changes

--- disturber-jar/src/main/java/com/dmytrobilokha/disturber/network/dto/JoinedRoomDto.java
+++ disturber-jar/src/main/java/com/dmytrobilokha/disturber/network/dto/JoinedRoomDto.java
@@ -1,10 +1,12 @@
 package com.dmytrobilokha.disturber.network.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
  * The matrix Joined Room DTO
  */
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class JoinedRoomDto {
 
     @JsonProperty(value = "unread_notifications", required = true)
